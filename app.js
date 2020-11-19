94df63deae180a91288cb8909ce90030a31f8da9
njmnbb@gmail.com <njmnbb@gmail.com>
2020-11-18 22:16:03 -0600

    Added 'presence' to bot, changed all 'vars' to 'let', separated out config file values into individual variables, restructured command logic, attempted to more smoothly play songs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const config = require('./config.json');
+const {prefix, token, youtubeApiKey} = require('./config.json');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const https = require('https');
@@ -6,7 +6,14 @@ const ytdl = require('ytdl-core');
 const fs = require('fs');
 
 bot.on('ready', () => {
-	console.log(`Logged in as ${bot.user.username} #${bot.user.discriminator}`); 
+	bot.user.setPresence({
+		activity: {
+			name: 'Shrek videos',
+			type: 'STREAMING',
+			url: 'https://www.youtube.com/watch?v=GYcPeH6IY9A'
+		},
+		status: 'online'
+	});
 });
 
 bot.on('guildCreate', (guild) => {
@@ -15,10 +22,11 @@ bot.on('guildCreate', (guild) => {
 });
 
 bot.on('message', (msg) => {
-	var args = msg.content.replace(/\s+/g, ' ').split(' ', 2);
-	var voiceChannel = msg.member.voice.channel;
-	var textChannel = msg.channel;
-	var name = args[1];
+	let args = msg.content.replace(/\s+/g, ' ').split(' ', 2);
+	let voiceChannel = msg.member.voice.channel;
+	let textChannel = msg.channel;
+	let command = args[0];
+	let name = args[1];
 	
 	// Error logging with timestamp
 	process.on('uncaughtException', function (e) {
@@ -28,36 +36,31 @@ bot.on('message', (msg) => {
 					if (err) throw err;
 					console.log('Saved!');
 					process.exit(1);
-				});	
+		});	
 	});
 	
 	// Checking for valid args
-	if(args[0] !== '!birthday' || args[1] === undefined) return;
+	if(command === `${prefix}birthday` && name !== undefined) {
 
-	// If user says stop, leave voice channel to stop song
-	if(name === 'stop' && voiceChannel != undefined) {
-		voiceChannel.leave();
-		return;
+		// If user says stop, leave voice channel to stop song
+		if(name === 'stop' && voiceChannel != undefined) {
+			voiceChannel.leave();
+			return;
+		} else if(name === 'help') {
+			textChannel.send('`!birthday [name]`: Searches the 1HappyBirthday channel for a song that matches the name entered\n`!birthday stop`: Stops the current song, if one is playing, and leaves the voice channel');
+			return;
+		} else if(voiceChannel === null) {
+			textChannel.send(msg.member.user.toString() + ' Please join a voice channel');
+			return;
+		} else {
+			// If no errors, search YouTube for video
+			getVideoId(voiceChannel, textChannel, name.toLowerCase());
+		}
 	}
-
-	if(name === 'help') {
-		textChannel.send('`!birthday [name]`: Searches the 1HappyBirthday channel for a song that matches the name entered\n`!birthday stop`: Stops the current song, if one is playing, and leaves the voice channel');
-
-		return;
-	}
-
-	// Checking if user is in a voice channel
-	if(voiceChannel === null) {
-		textChannel.send(msg.member.user.toString() + ' Please join a voice channel');
-		return;
-	}
-
-	// If no errors, search YouTube for video
-	getVideoId(voiceChannel, textChannel, name.toLowerCase());
 
 });
 
-bot.login(config.token);
+bot.login(token);
 
 /************
 * FUNCTIONS
@@ -65,12 +68,12 @@ bot.login(config.token);
 
 function playSound(voiceChannel, videoId) {
 	const url = 'http://youtube.com/watch?v=' + videoId;
-	var streamOptions = { seek: 0, volume: 0.3 };
+	let streamOptions = { seek: 0, volume: 0.2, bitrate: 'auto' };
 
 	voiceChannel.join()
 		.then(connection => {
 			if(videoId !== undefined) {
-				const stream = ytdl(url, {filter : 'audioonly'});
+				const stream = ytdl(url, {filter: 'audioonly', quality: 'lowest'});
 				const dispatcher = connection.play(stream, streamOptions);
 
 				dispatcher.on('finish', () => voiceChannel.leave());
@@ -81,9 +84,9 @@ function playSound(voiceChannel, videoId) {
 
 
 function getVideoId(voiceChannel, textChannel, name) {
-	var vidId = '';
-	var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCIJ5p0TQwRyBgdq6Wb7D_gA&maxResults=5&q=' + name + '&type=video&key=' + config.key;
-	var titles = '';
+	let vidId = '';
+	let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCIJ5p0TQwRyBgdq6Wb7D_gA&maxResults=5&q=' + name + '&type=video&key=' + youtubeApiKey;
+	let titles = '';
 
 	// Using YouTube API to search 1HappyBirthday's channel for the appropriate video
 	https.get(url, (res) => {
